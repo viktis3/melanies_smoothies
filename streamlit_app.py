1c76a487859361c63d45b5afbd86bdee18a2607e
viktis3 <isaksenviktor2@gmail.com>
2026-08-29 13:19:12 +0200

    Modify dataframe selection and add search functionality
    Updated the dataframe selection to include 'FRUIT_NAME' and 'SEARCH_ON'. Added functionality to retrieve search values for selected fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,13 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 # Convert the snowspark dataframe to pandas
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
 name_on_order = st.text_input('Please input your name:')
 st.write('The name on your smoothie will be:', name_on_order)
@@ -35,6 +34,10 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 

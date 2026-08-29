2765b8a77927e0901bd14584ea63781148a6d1ce
viktis3 <isaksenviktor2@gmail.com>
2026-08-29 13:21:05 +0200

    Update API endpoint for fruit nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredient_list:
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-      
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)

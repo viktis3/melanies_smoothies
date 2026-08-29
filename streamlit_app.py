c6c98cfd8e0bf5358d54b4af57bd6420bbf3eddb
viktis3 <isaksenviktor2@gmail.com>
2026-08-29 12:58:42 +0200

    Update API call to use selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)

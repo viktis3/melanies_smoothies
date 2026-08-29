d226cd6a74deff361a560a30d9512d92bf6eb83c
viktis3 <isaksenviktor2@gmail.com>
2026-08-29 12:56:21 +0200

    Comment out text display and add dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ if ingredient_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

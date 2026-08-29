726f9331344e4b0c01ac9c0f654a25da2091d972
viktis3 <isaksenviktor2@gmail.com>
2026-08-29 12:58:05 +0200

    Refactor smoothie order submission and cleanup
    Removed commented-out code and adjusted smoothie order insertion logic.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -45,6 +45,3 @@ if ingredient_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
